7d5d47dbe8cc858ddf3b0e3e5e8740297f1f226d
Marsonn07 <rafaelmarson.rafa@gmail.com>
2024-11-21 14:12:48 -0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } 
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Matricula> Matriculas { get; set; }
+    public DbSet<Projeto> Projetos { get; set; }
+    public DbSet<Funcionario> Funcionarios { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Movimentacao> Movimentacoes { get; set; }
+    public DbSet<LogMovimentacao> LogsMovimentacao { get; set; }
+    public DbSet<CategoriaProduto> CategoriaProdutos { get; set; }
+    public DbSet<Vendas> Vendas { get; set; }
+}
